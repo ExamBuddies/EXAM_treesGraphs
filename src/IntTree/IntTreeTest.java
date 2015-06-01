b94b6a2a9a94331c802b75bfff9e01c4e038e142
DAT14V1MadsPelt <mads3166@stud.kea.dk>
2015-06-01 11:38:34 +0200

    first commit intTree

---
+++ src/IntTree/IntTreeTest.java
@@ -0,0 +1,17 @@
+package IntTree;
+
+public class IntTreeTest {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IntTree fem =new IntTree(30);
+		fem.print();
+		fem.printInorder();
+		fem.printPostorder();
+		fem.printPreorder();
+		fem.printSideways();
+	}
+
+}
